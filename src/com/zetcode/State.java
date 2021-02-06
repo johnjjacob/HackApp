88f7cd1059d47e37685a977daaa9512f566a40c4
jpatel23 <jaiminabpatel@ou.edu>
2021-02-06 16:32:26 -0600

    Update

--- src/com/zetcode/State.java
+++ src/com/zetcode/State.java
@@ -1,37 +1,11 @@
 package com.zetcode;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
 public class State {
-	
+
 	public State(String name) {
 		
 		System.out.println(name);
 		
 	}
 	
-	
-
-	public static void main(String[] args) throws IOException, InterruptedException {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
-		System.out.println("test");
-		
-		
-		
-		HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.covidtracking.com/v1/us/daily.json"))
-                .build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        System.out.println(response.body());
-	}
-
 }
