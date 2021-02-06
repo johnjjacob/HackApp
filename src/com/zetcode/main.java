499a9a0ae5c50303153be3a9411ec7c484ec9fa3
jpatel23 <jaiminabpatel@ou.edu>
2021-02-06 17:51:39 -0600

    Object mapper

--- src/com/zetcode/main.java
+++ src/com/zetcode/main.java
@@ -1,30 +1,53 @@
 package com.zetcode;
 
 import java.io.IOException;
+import java.lang.Object;
 import java.util.HashMap;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class main {
 	
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
-		System.out.println("test");
+	
 		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		
+		System.out.print("Which state would you like?");
+		String state = scanner.nextLine();
+		
+		String url = "https://api.covidtracking.com/v1/states/"+state+"/daily.json";
 		
 		
 		HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.covidtracking.com/v1/us/daily.json"))
+                .uri(URI.create(url))
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         System.out.println(response.body());
+        
+       
+        
+        
+        Data one = new Data();
+        
+      //  one.dataSet =  new ObjectMapper().readValue(response.body(), HashMap.class);
+        
+        
+        
+        
+        
 	}
 
 }
