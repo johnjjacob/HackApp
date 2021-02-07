5ee9b5677292023cdfa567c1de3db279bea8b7cd
Aruksh Patel <arukshpatel98@gmail.com>
2021-02-06 20:19:05 -0600

    GUI update

--- GUI/src/Window.java
+++ GUI/src/Window.java
@@ -1,16 +1,19 @@
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.TabFolder;
-import org.eclipse.swt.widgets.TabItem;
-import org.eclipse.swt.widgets.DateTime;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
 
 public class Window {
+	
+	public String[] states = {"AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC",  
+			"DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA",  
+			"MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE",  
+			"NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC",  
+			"SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"};
 
 	/**
 	 * Launch the application.
@@ -31,14 +34,29 @@ public class Window {
 	public void open() {
 		
 		Display display = Display.getDefault();
+		
 		int displayHeight = display.getPrimaryMonitor().getBounds().height;
 		int displayWidth = display.getPrimaryMonitor().getBounds().width;
+		
 		Shell shell = new Shell();
 		shell.setSize(displayWidth/2, displayHeight/2);
 		shell.setText("COVID Application");
 		
-		DateTime dateTime = new DateTime(shell, SWT.BORDER);
-		dateTime.setSize(836, 491);
+		
+		
+		
+		Label lblChooseDate = new Label(shell, SWT.NONE);
+		lblChooseDate.setBounds(10, 0, 92, 14);
+		lblChooseDate.setText("Choose State");
+		
+		Tree tree = new Tree(shell, SWT.BORDER);
+		tree.setBounds(10, 25, 141, 434);
+		
+		for(int i = 0; i < states.length; i++)
+		{
+			TreeItem trtmState = new TreeItem(tree, SWT.NONE);
+			trtmState.setText(states[i]);
+		}
 
 		shell.open();
 		shell.layout();
