f35a950bb1177b63a3efd45ef7b08a8ff60934c5
johnjjacob <43150214+johnjjacob@users.noreply.github.com>
2021-02-07 00:56:05 -0600

    Added filter for zeros again

--- src/com/zetcode/main.java
+++ src/com/zetcode/main.java
@@ -78,9 +78,12 @@ public class main {
         for(int i = 0; i < dataset.header.size();i++)
         {
         	
-        	if(dataset.dataVal.get(i) != "" && i !=39 && i != 48 && i != 54 && i != 49 && i != 50 && i != 51 && i != 52 && i != 53)
+        	if(dataset.dataVal.get(i) != "" && i !=39 && i != 48 && i != 54 &&i != 40)
         	{
-                System.out.println(dataset.header.get(i)+" "+dataset.dataVal.get(i));
+        		if(!dataset.dataVal.get(i).equals("0")) {
+        			 System.out.println(dataset.header.get(i)+" "+dataset.dataVal.get(i));
+        		}
+               
         	}
 
         }
@@ -96,11 +99,13 @@ public class main {
         
         for(int i = 0; i < dataset.header.size();i++)
         {
-        	if(dataset.dataVal.get(i) != "" && i !=39 && i != 48 && i != 54 && i != 49 && i != 50 && i != 51 && i != 52 && i != 53)
+        	if(dataset.dataVal.get(i) != "" && i !=39 && i != 48 && i != 54 &&i != 40)
         	{
+        		if(!dataset.dataVal.get(i).equals("0")) {
+
                 column.add(dataset.header.get(i));
                 j++;
-        	
+        		}
         	}
         }
         
@@ -110,11 +115,13 @@ public class main {
         
          for(int i = 0; i < dataset.header.size();i++)
          {
-            if(dataset.dataVal.get(i) != "" && i !=39 && i != 48 && i != 54 && i != 49 && i != 50 && i != 51 && i != 52 && i != 53)
+         	if(dataset.dataVal.get(i) != "" && i !=39 && i != 48 && i != 54 &&i != 40)
              {
+        		if(!dataset.dataVal.get(i).equals("0")) {
+
                  data.add(dataset.dataVal.get(i));
                   j++;
-             	
+        		}
             }
          }
          
