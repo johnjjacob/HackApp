7e2b92db27781c341b6777210a87942b69864339
David Aycok <47369071+Kingdavid2019@users.noreply.github.com>
2021-02-06 20:51:00 -0600

    Update main.java
    No zeros printed

--- src/com/zetcode/main.java
+++ src/com/zetcode/main.java
@@ -20,10 +20,10 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 
 
-public class main {
+public class Main {
 	
 
 	public static void main(String[] args) throws IOException, InterruptedException {
@@ -59,9 +59,12 @@ public class main {
         for(int i = 0; i < dataset.header.size();i++)
         {
         	
-        	if(dataset.dataVal.get(i) != "" && i !=39 && i != 48 && i != 54 && i != 49 && i != 50 && i != 51 && i != 52 && i != 53)
+        	if(dataset.dataVal.get(i) != "" && i !=39 && i != 48 && i != 54 &&i != 40)
         	{
-                System.out.println(dataset.header.get(i)+" "+dataset.dataVal.get(i));
+        		if(!dataset.dataVal.get(i).equals("0")) {
+        			 System.out.println(dataset.header.get(i)+" "+dataset.dataVal.get(i));
+        		}
+               
         	}
 
         }
